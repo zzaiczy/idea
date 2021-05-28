96607d30ea3aa0cbe3316a7056ff5f727d561074
zzaiczy <zhangzhenbit@gmail.com>
2021-05-28 21:00:25 +0800

    finishrotate

---
+++ likou/test.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3]
+print(list(x+1 for x in a))
+
+
+
--- likou/train_01.py
+++ likou/train_01.py
@@ -1,27 +1,30 @@
+# 矩阵的旋转
 class Solution:
     def rotate1(self, matrix):
         n = len(matrix)
         delta = n//2
-        if not n % 2:
-            even = 1
+        if n % 2:
+            odd = 1
+            tk = 0
         else:
-            even = 0
-        # 旋转下去再提上来
-        for i in range(n):
-            for j in range(n):
-                pass
-        for i in range(1, delta+1):
-            for j in range(1, delta+1):
-                first_i, first_j = i+delta-1, j+delta-1
-                second_i, second_j = j+delta-1, -i+delta-1
-                third_i, third_j = -i+delta-1, -j+delta-1
-                fourth_i, four_j = -j+delta-1, i+delta-1
-                print(first_i, first_j, second_i, second_j, third_i, third_j, fourth_i, four_j)
+            odd = 0
+            delta = 2*delta
+            tk = 1
+        for i in range(1, delta+1, tk+1):
+            for j in range(tk, delta+1, tk+1):
+                first = [i, j]
+                second = [j, -i]
+                third = [-i, -j]
+                fourth = [-j, i]
+                first_i, first_j = (first[0]+delta-tk)//(tk+1), (first[1]+delta-tk)//(tk+1)
+                second_i, second_j = (second[0]+delta-tk)//(tk+1), (second[1]+delta-tk)//(tk+1)
+                third_i, third_j = (third[0]+delta-tk)//(tk+1), (third[1]+delta-tk)//(tk+1)
+                fourth_i, four_j = (fourth[0]+delta-tk)//(tk+1), (fourth[1]+delta-tk)//(tk+1)
                 matrix[first_i][first_j], matrix[second_i][second_j], matrix[third_i][third_j], matrix[fourth_i][four_j] = matrix[fourth_i][four_j], matrix[first_i][first_j], matrix[second_i][second_j], matrix[third_i][third_j]
         return matrix
 
 
 a = Solution()
-b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-c = [[1, 2], [3, 4]]
-print(a.rotate1(c))
+b = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
+c = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+print(a.rotate1(b))
